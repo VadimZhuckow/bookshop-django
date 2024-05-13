9f4948d0ddfe13ea0e1e8036cecf1b6919ac50b1
VadimZhuckow <apex69knight@gmail.com>
2024-05-13 22:24:02 +0300

    add new handler

--- books/urls.py
+++ books/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("current-time/", views.current_time),
     path("books/random-book", views.random_book),
-    path("books/", views.view_all_book)
+    path("books/", views.view_all_book),
+    path("books/<int:book_id>", views.get_one_book),
+    path("books/categories", views.get_categories),
+    path("books/categories/<slug:category_slug>", views.get_book_by_category)
 ]
--- books/views.py
+++ books/views.py
@@ -2,8 +2,12 @@ from datetime import datetime
 from random import choice
 
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseNotFound
-from books.models import BOOKS
+from django.http import HttpRequest, HttpResponse, JsonResponse, Http404
+from books.models import BOOKS, CATEGORIES
+
+
+def custom_404(request: HttpRequest, exception) -> HttpResponse:
+    return HttpResponse('упс 404', status=404)
 
 
 def current_time(request: HttpRequest) -> HttpResponse:
@@ -27,10 +31,18 @@ def random_book(request: HttpRequest) -> HttpResponse:
 
 
 def view_all_book(request):
+    query_param = request.GET
+    query_published = query_param.get('published_year')
 
-    
+    books = BOOKS.copy()
+    if query_published is not None:
+        query_published = int(query_published)
+        books = [book
+                 for book in books
+                 if book['published_year'] == query_published
+                 ]
     return JsonResponse(
-        BOOKS,
+        books,
         safe=False,
         json_dumps_params={
             "indent": 4,
@@ -39,5 +51,37 @@ def view_all_book(request):
     )
 
 
-def custom_404(request: HttpRequest, exception) -> HttpResponse:
-    return HttpResponse('упс 404', status=404)
+def get_categories(request: HttpRequest) -> HttpResponse:
+    return JsonResponse(
+        CATEGORIES,
+        safe=False,
+        json_dumps_params={
+            "indent": 4,
+            "ensure_ascii": False
+        }
+    )
+
+
+def get_one_book(request: HttpRequest, book_id: int) -> HttpResponse:
+    for book in BOOKS:
+        if book['id'] == book_id:
+            return JsonResponse(
+                book,
+                json_dumps_params={
+                    'indent': 4,
+                    'ensure_ascii': False
+                }
+            )
+    raise Http404('Не верный id')
+
+
+def get_book_by_category(request: HttpRequest, category_slug: str) -> HttpResponse:
+
+    return JsonResponse(
+        [book for book in BOOKS if book["category"] == category_slug],
+        safe=False,
+        json_dumps_params={
+            "indent": 4,
+            "ensure_ascii": False
+        }
+    )
